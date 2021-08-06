10f4cd05a07f0f36f7562b348155b4ecd0761bdf
Alan Berdinelli <alan.berdinelli@clinifyhealth.com>
2021-08-06 03:05:54 -0300

    Added error handling to process img

--- src/lambdas/processImage.js
+++ src/lambdas/processImage.js
@@ -6,16 +6,31 @@ const allowedTypes = ['image/jpg','image/jpeg','image/png'];
 async function processImage(event) {
     const s3 = S3();
     const [ record ] = event.Records;
+    
+    let Body, ContentType;
+
+    console.log(record);
+
     const sizes = [
         { width: 150, height: 150 }, 
         { width: 100, height: 100 }, 
         { width: 50, height: 50 }
     ];
 
-    const { Body, ContentType } = await s3.getObject({
-        Bucket: record.s3.bucket.name,
-        Key: record.s3.object.key
-    }).promise();
+    try {
+        const object = await s3.getObject({
+            Bucket: record.s3.bucket.name,
+            Key: record.s3.object.key
+        }).promise();
+    
+        Body = object.Body;
+        ContentType = object.ContentType;
+    }
+    catch (err) {
+        console.log(err);
+
+        return { statusCode: 500, body: 'Could not get object' };
+    }
 
     if (!allowedTypes.includes(ContentType)) {
         return {
